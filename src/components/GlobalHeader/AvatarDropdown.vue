9a5310ecdb9c6010a2c84d01837510932c884fbe
wxb <wxbing1207@163.com>
2020-08-03 20:24:12 +0800

    公共header 头像用户名

--- src/components/GlobalHeader/AvatarDropdown.vue
+++ src/components/GlobalHeader/AvatarDropdown.vue
@@ -1,18 +1,14 @@
 <template>
-  <a-dropdown v-if="currentUser && currentUser.name" placement="bottomRight">
+  <a-dropdown v-if="$store.state.user" placement="bottomRight">
     <span class="ant-pro-account-avatar">
-      <a-avatar size="small" src="https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png" class="antd-pro-global-header-index-avatar" />
-      <span>{{ currentUser.name }}</span>
+      <a-avatar size="small" :src="$store.state.user.avatar" class="antd-pro-global-header-index-avatar" />
+      <span>{{ $store.state.user.name }}</span>
     </span>
     <template v-slot:overlay>
       <a-menu class="ant-pro-drop-down menu" :selected-keys="[]">
         <a-menu-item v-if="menu" key="center" @click="handleToCenter">
           <a-icon type="user" />
           个人中心
-        </a-menu-item>
-        <a-menu-item v-if="menu" key="settings" @click="handleToSettings">
-          <a-icon type="setting" />
-          个人设置
         </a-menu-item>
         <a-menu-divider v-if="menu" />
         <a-menu-item key="logout" @click="handleLogout">
